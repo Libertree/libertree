bc4a10b7d35d90156ac584b0cdf3c3e93c3b429a
Pistos <github.pistos@purepistos.net>
2012-03-20 16:37:10 -0400

    Responder::Authentication (basics).

--- backend/rb/spec/responder_authentication_spec.rb
+++ backend/rb/spec/responder_authentication_spec.rb
@@ -9,11 +9,17 @@ describe Libertree::Server::Responder::Authentication do
     it 'returns OK when the public_key is unrecognized' do
       @s.process 'INTRODUCE { "public_key": "some brand new public key"}'
       shouldda_responded_with_code 'OK'
+      response['challenge'].should be_nil
     end
 
     describe 'when the public_key is recognized' do
-      it 'returns OK and challenges the requester ' do
-        @s.process 'INTRODUCE { "public_key": "known public key"}'
+      before :each do
+        @requester = Libertree::Model::Server.create( FactoryGirl.attributes_for(:server) )
+      end
+
+      it 'returns OK and challenges the requester' do
+        params = { 'public_key' => @requester.public_key }.to_json
+        @s.process "INTRODUCE #{params}"
         shouldda_responded_with_code 'OK'
         response['challenge'].should =~ /^\S{16,}$/
       end
