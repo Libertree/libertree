bb0be4d5b54657b2f3a091cbd74be22bc6a244fe
Pistos <github.pistos@purepistos.net>
2012-03-23 19:47:44 -0400

    Bcrypt for account passwords.

--- rb/lib/libertree/model/account.rb
+++ rb/lib/libertree/model/account.rb
@@ -1,6 +1,28 @@
+require 'bcrypt'
+
 module Libertree
   module Model
     class Account < M4DBI::Model(:accounts)
+
+      # These two password methods provide a seamless interface to the BCrypted
+      # password.  The pseudo-field "password" can be treated like a normal
+      # String field for reading and writing.
+      def password
+        @password ||= BCrypt::Password.new(password_hash)
+      end
+
+      def password=( new_password )
+        @password = BCrypt::Password.create(new_password)
+        self.password_hash = @password
+      end
+
+      # @return [Account] authenticated account, or nil on failure to authenticate
+      def self.authenticate(creds)
+        account = Account[ 'username' => creds['username'] ]
+        if account.password ==  creds['password']
+          account
+        end
+      end
     end
   end
 end
