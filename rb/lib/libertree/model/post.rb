d7171056cc8724473c2d349684bb8732ef9d2ea0
Pistos <github.pistos@purepistos.net>
2012-03-28 16:04:39 -0400

    Markdown!

--- rb/lib/libertree/model/post.rb
+++ rb/lib/libertree/model/post.rb
@@ -1,8 +1,19 @@
+require 'redcarpet'
+
 module Libertree
   module Model
     class Post < M4DBI::Model(:posts)
       def member
         Member[self.member_id]
+      end
+
+      def text_rendered
+        markdown = Redcarpet::Markdown.new(
+          Redcarpet::Render::HTML,
+          autolink: true,
+          space_after_headers: true
+        )
+        markdown.render text
       end
     end
   end
