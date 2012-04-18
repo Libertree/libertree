6bf50fda7398ea0a53b9e415c140a0da35b0f9f3
Pistos <github.pistos@purepistos.net>
2012-04-17 23:59:10 -0400

    Run specs only in "test" environment.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,11 @@ class MockServer
   end
 end
 
+if ENV['LIBERTREE_ENV'] != 'test'
+  $stderr.puts "Refusing to run specs in a non-test environment.  Comment out the exit line if you know what you're doing."
+  exit 1
+end
+
 RSpec.configure do |config|
   config.before(:each) do
     @s = MockServer.new
