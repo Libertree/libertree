5eaac082f50f4f72cce5a7ae9dd543f3d4c9b9ae
Pistos <github.pistos@purepistos.net>
2012-04-30 09:39:46 -0400

    Remove RCrypt reference from factory definition (replace with OpenSSL).

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
       m = n / 256  # integer division
       "192.168.#{m}.#{n}"
     }
-    public_key RCrypt.generate_key_pair[:public]
+    public_key OpenSSL::PKey::RSA.new(2048, 65537).public_key.to_pem
     # sequence(:public_key, 0xf000000) { |n| "%08x" % n }
   end
 
