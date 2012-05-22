15e2905534664c44bd9f5ac699418cec20a91457
rekado <rekado@elephly.net>
2012-05-23 06:54:36 +0800

    create UTF8 encoded dbs by default

--- createdb.sh
+++ createdb.sh
@@ -1,3 +1,3 @@
-createdb -U postgres -O libertree libertree_development
-createdb -U postgres -O libertree libertree_test
-createdb -U postgres -O libertree libertree_production
+createdb -U postgres -O libertree -E UTF8 libertree_development
+createdb -U postgres -O libertree -E UTF8 libertree_test
+createdb -U postgres -O libertree -E UTF8 libertree_production
