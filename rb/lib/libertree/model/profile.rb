230ac468bf4c88eab5c93bbebea831181c0f476b
Pistos <github.pistos@purepistos.net>
2012-06-17 01:11:51 -0400

    Profile.search .

--- rb/lib/libertree/model/profile.rb
+++ rb/lib/libertree/model/profile.rb
@@ -4,6 +4,10 @@ module Libertree
       def member
         Member[ self.member_id ]
       end
+
+      def self.search(query)
+        s  "SELECT * FROM profiles WHERE name_display ILIKE '%' || ? || '%' OR description ILIKE '%' || ? || '%'", query, query
+      end
     end
   end
 end
