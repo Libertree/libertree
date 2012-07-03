73ad4877a3fd5880ca68dcb8f85324eec2fbc731
rekado <rekado@elephly.net>
2012-07-03 09:15:59 +0800

    extend instead of register/include

--- bin/job-processor.rb
+++ bin/job-processor.rb
@@ -1,10 +1,11 @@
-require 'job-processor'
-require 'jobs'
+require 'libertree/job-processor'
+require_relative '../lib/jobs'
 
 if ARGV[0].nil?
   $stderr.puts "#{$0} <config.yaml>"
   exit 1
 end
 
-JobProcessor.register Tasks
-JobProcessor.new( ARGV[0], "test" ).run
+jobp = JobProcessor.new( ARGV[0], "test" )
+jobp.extend Jobs
+jobp.run
