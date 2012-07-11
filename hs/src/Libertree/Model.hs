7825cd174fca1a53076ff0eff39209938fd8f345
rekado <rekado@elephly.net>
2012-07-11 11:00:05 +0800

    minimal model for post_likes

--- hs/src/Libertree/Model.hs
+++ hs/src/Libertree/Model.hs
@@ -2,6 +2,7 @@ module Libertree.Model
   ( module Model )
 where
 
-import Libertree.Model.Common as Model
-import Libertree.Model.Comment as Model
-import Libertree.Model.Post as Model
+import Libertree.Model.Common   as Model
+import Libertree.Model.Comment  as Model
+import Libertree.Model.Post     as Model
+import Libertree.Model.PostLike as Model
