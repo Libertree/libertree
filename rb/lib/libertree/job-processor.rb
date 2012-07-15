bc59715cd800c997466532c764349cf8ba895a47
rekado <rekado@elephly.net>
2012-07-15 20:03:57 +0800

    manual merge with latest job model changes

--- rb/lib/libertree/job-processor.rb
+++ rb/lib/libertree/job-processor.rb
@@ -1,9 +1,11 @@
 module Libertree
   class JobProcessor
-    def initialize(log_path, queue)
-      @queue = queue
-      if log_path
-        @log = File.open( log_path, 'a+' )
+    def initialize(config_filename)
+      @config_filename = config_filename
+      @conf = YAML.load( File.read(config_filename) )
+
+      if @conf['log_path']
+        @log = File.open( @conf['log_path'], 'a+' )
         @log.sync = true
       else
         @log = $stdout
@@ -14,6 +16,8 @@ module Libertree
       if @log.respond_to? :path
         puts "pid #{@pid} logging to #{File.absolute_path(@log.path)}"
       end
+
+      @queue = @conf['queue']
     end
 
     def log(s, level = nil)
@@ -29,28 +33,6 @@ module Libertree
       log s, 'ERROR'
     end
 
-    # @return [Job] nil if no job was reserved
-    def reserve
-      job = Libertree::Model::Job.s1("SELECT * FROM jobs WHERE queue = '#{@queue}' AND pid IS NULL AND tries < 5 AND time_to_start <= NOW() ORDER BY time_to_start ASC LIMIT 1")
-      return  if job.nil?
-
-      Libertree::Model::Job.update(
-        {
-          id: job.id,
-          pid: nil,
-        },
-        {
-          pid: Process.pid,
-          time_started: Time.now
-        }
-      )
-
-      job = Libertree::Model::Job[job.id]
-      if job.pid == Process.pid
-        job
-      end
-    end
-
     def run
       quit = false
 
@@ -62,11 +44,13 @@ module Libertree
       Signal.trap("INT" , &terminate)
 
       until quit
-        job = reserve
+        job = Libertree::Model::Job.reserve(@queue)
         if job
           process job
         end
-        3.times { sleep 1 unless quit }
+        3.times do
+          sleep 1 unless quit
+        end
       end
     end
 
@@ -80,19 +64,16 @@ module Libertree
           job.time_finished = Time.now
         else
           # Return job to queue.
-          job.set(
-            time_started: nil,
-            pid: nil,
-            tries: job.tries + 1,
-            time_to_start: Time.now + 60*5
-          )
+          job.unreserve
         end
 
         log "Leaving: job #{job.id}"
       rescue NameError
         log_error "Skipping unknown task #{job.task} in job #{job.id}"
-      rescue Exception => e
+        job.unreserve
+      rescue StandardError => e
         log_error "Error processing job #{job.id}: #{e.class} #{e.message}\n" + e.backtrace.join("\n\t")
+        job.unreserve
       end
     end
   end
