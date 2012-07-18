61554f1b9ac8b229cb3f5382765fabefbcde4d7d
rekado <rekado@elephly.net>
2012-07-18 20:03:05 +0800

    filter by owner of like, not owner of associated post

--- lib/libertree/server/responder/comment-like.rb
+++ lib/libertree/server/responder/comment-like.rb
@@ -65,7 +65,7 @@ module Libertree
           begin
             likes = Model::CommentLike.
               where( 'remote_id' => params['id'] ).
-              reject { |c| c.comment.server != @server }
+              keep_if { |like| like.member.server == @server }
 
             if likes.empty?
               respond( {
--- lib/libertree/server/responder/post-like.rb
+++ lib/libertree/server/responder/post-like.rb
@@ -65,7 +65,7 @@ module Libertree
           begin
             likes = Model::PostLike.
               where( 'remote_id' => params['id'] ).
-              reject { |c| c.post.server != @server }
+              keep_if { |like| like.member.server == @server }
 
             if likes.empty?
               respond( {
