4cfcd37b0426659d5c8304506527fc5cc82802bf
rekado <rekado@elephly.net>
2012-08-13 12:11:13 +0800

    move Haskell bindings to separate repository

--- hs/src/Libertree/Model.hs
+++
@@ -1,9 +0,0 @@
-module Libertree.Model
-  ( module Model )
-where
-
-import Libertree.Model.Common      as Model
-import Libertree.Model.Comment     as Model
-import Libertree.Model.CommentLike as Model
-import Libertree.Model.Post        as Model
-import Libertree.Model.PostLike    as Model
