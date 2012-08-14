5bdb305605f5b0261917677a615e294c6ea41d85
rekado <rekado@elephly.net>
2012-08-14 15:24:25 +0800

    send "public: true" for backwards compatibility

--- lib/libertree/client.rb
+++ lib/libertree/client.rb
@@ -148,6 +148,7 @@ module Libertree
         'username'   => post.member.username,
         'id'         => post.id,
         'visibility' => post.visibility,
+        'public'     => true, # FIXME: backwards compatibility
         'text'       => post.text
       }
       params.merge!('references' => references) unless references.empty?
