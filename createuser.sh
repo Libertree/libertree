b89e0b4501a6e83d4c83aec85cfc95987cc5f11e
rekado <rekado@elephly.net>
2012-12-01 12:35:20 +0800

    check if user exists before creating

--- createuser.sh
+++ createuser.sh
@@ -1,1 +1,5 @@
-createuser -S -D -R -U postgres libertree
+if echo "\dg" | psql -U postgres | grep -wq libertree; then
+  echo "User already exists: libertree"
+else
+  createuser -S -D -R -U postgres libertree
+fi
