88f603a0540df491dedb6b4dd4268bb30a3453a1
rekado <rekado@elephly.net>
2012-12-01 12:35:44 +0800

    check if db already exists

--- createdb.sh
+++ createdb.sh
@@ -1,3 +1,7 @@
-createdb -U postgres -O libertree -E UTF8 libertree_development
-createdb -U postgres -O libertree -E UTF8 libertree_test
-createdb -U postgres -O libertree -E UTF8 libertree_production
+for db in "test" "development" "production"; do
+  if psql -U postgres -l | grep -wq "libertree_$db"; then
+    echo "Already exists: libertree_$db"
+  else
+    createdb -U postgres -O libertree -E UTF8 "libertree_$db"
+  fi
+done
