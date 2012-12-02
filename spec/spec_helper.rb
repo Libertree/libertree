704b0ecc90e03ee8a9efd4a57a062e38e51b3269
rekado <rekado@elephly.net>
2012-12-02 13:12:12 +0800

    tests: initialise db connection before loading libertree/server

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,14 @@
 require 'ruby-debug'
+require 'libertree/db'
+
+########################
+# FIXME: M4DBI wants us to connect to the db before defining models.  As model
+# definitions are loaded when 'libertree/server' is required, we have to do
+# this first.
+Libertree::DB.load_config "#{File.dirname( __FILE__ ) }/../database.yaml"
+Libertree::DB.dbh
+########################
+
 require 'libertree/server'
 require_relative 'factories'
 
