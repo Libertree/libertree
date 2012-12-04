95d68eb1682681ef164ba58a08ff604aa1075a93
Pistos <github.pistos@purepistos.net>
2012-12-03 19:48:38 -0500

    Added spec for #delete_cascade .

---
+++ spec/model/account_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe Libertree::Model::Account do
+  before :each do
+    @account = Libertree::Model::Account.create( FactoryGirl.attributes_for(:account) )
+    @member = @account.member
+  end
+
+  describe '#delete_cascade' do
+    context 'given an account with some posts and other entities' do
+      before :each do
+        other_account = Libertree::Model::Account.create( FactoryGirl.attributes_for(:account) )
+        other_member = other_account.member
+
+        post1 = Libertree::Model::Post.create(
+          FactoryGirl.attributes_for( :post, member_id: @member.id, text: 'first post' )
+        )
+        post2 = Libertree::Model::Post.create(
+          FactoryGirl.attributes_for( :post, member_id: other_member.id, text: 'second post' )
+        )
+
+        comment1 = Libertree::Model::Comment.create(
+          FactoryGirl.attributes_for( :comment, member_id: @member.id, post_id: post2.id, text: 'first comment' )
+        )
+        comment2 = Libertree::Model::Comment.create(
+          FactoryGirl.attributes_for( :comment, member_id: other_member.id, post_id: post2.id, text: 'second comment' )
+        )
+
+        @post1_id = post1.id
+        @post2_id = post2.id
+        @comment1_id = comment1.id
+        @comment2_id = comment2.id
+        @member_id = @member.id
+        @account_id = @account.id
+      end
+
+      it 'deletes the account and all subordinate entities belonging to the account, but not entities not belonging to the account' do
+        Libertree::Model::Post[@post1_id].should_not be_nil
+        Libertree::Model::Post[@post2_id].should_not be_nil
+        Libertree::Model::Comment[@comment1_id].should_not be_nil
+        Libertree::Model::Comment[@comment2_id].should_not be_nil
+        Libertree::Model::Member[@member_id].should_not be_nil
+        Libertree::Model::Account[@account_id].should_not be_nil
+
+        @account.delete_cascade
+
+        Libertree::Model::Post[@post1_id].should be_nil
+        Libertree::Model::Post[@post2_id].should_not be_nil
+        Libertree::Model::Comment[@comment1_id].should be_nil
+        Libertree::Model::Comment[@comment2_id].should_not be_nil
+        Libertree::Model::Member[@member_id].should be_nil
+        Libertree::Model::Account[@account_id].should be_nil
+      end
+    end
+  end
+end
