d88812342900ab4572ece82b56f70f95d610efe1
rekado <rekado@elephly.net>
2013-01-07 07:45:03 +0800

    test for build_stanza

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -6,6 +6,30 @@ describe Libertree::Connection do
     @c = Libertree::Connection.new({})
   end
 
+  describe 'build_stanza' do
+    it 'constructs a valid iq stanza' do
+      stanza = @c.build_stanza(
+        'libertree.localhost.localdomain',
+        'POST',
+        {
+          'text' => 'Hello',
+          'id'   => 123
+        }
+      )
+      example =<<HERE
+<iq type="get" to="libertree.localhost.localdomain" id="blather0001">
+  <libertree>
+    <post>
+      <text>Hello</text>
+      <id>123</id>
+    </post>
+  </libertree>
+</iq>
+HERE
+      stanza.to_xml.should == Nokogiri::XML::fragment(example).to_xml.strip
+    end
+  end
+
   describe 'params_to_xml' do
     it 'constructs a valid xml fragment' do
       xml = @c.params_to_xml(
