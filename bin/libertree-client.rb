adae0c3c64aacfa2b51068228cce57d0c61b0333
rekado <rekado@elephly.net>
2013-01-31 18:24:32 +0800

    connect to socket and catch connection errors

--- bin/libertree-client.rb
+++ bin/libertree-client.rb
@@ -12,31 +12,40 @@ if ARGV.size < 1
 end
 
 key = OpenSSL::PKey::RSA.new File.read(ARGV[0])
-client = Libertree::Client.new(
-  private_key: key,
-  public_key: key.public_key.to_pem,
-  avatar_url_base: ARGV[2]
-)
+socket = '/tmp/libertree-relay'
 
-while input = Readline.readline("libertree> ", true)
-  next  if input.nil? || input.empty?
+begin
+  client = Libertree::Client.new(
+    private_key: key,
+    public_key: key.public_key.to_pem,
+    avatar_url_base: ARGV[2],
+    socket: socket
+  )
 
-  command, *params = input.split(/\s+/)
-  param = params[0]
-  case command.downcase
-  when /^c/  # connect
-    client.connect param
-  when /^f/  # forest
-    forest = Forest.new(
-      rand(99),
-      params[0],
-      params[1..-1].map { |p|
-        Tree.new(p)
-      }
-    )
-    client.req_forest forest
-  when /^m/  # member
-    member = Member.new(params[0], params[1])
-    client.req_member member
+  while input = Readline.readline("libertree> ", true)
+    next  if input.nil? || input.empty?
+
+    command, *params = input.split(/\s+/)
+    param = params[0]
+    case command.downcase
+    when /^f/  # forest
+      forest = Forest.new(
+        rand(99),
+        params[0],
+        params[1..-1].map { |p|
+          Tree.new(p)
+        }
+      )
+      client.req_forest forest
+    when /^m/  # member
+      member = Member.new(params[0], params[1])
+      client.req_member member
+    end
   end
+rescue Errno::ECONNREFUSED
+  $stderr.puts "Failed to connect to socket at #{socket}"
+  exit 1
+rescue Errno::EPIPE
+  $stderr.puts "Broken pipe. Reconnecting."
+  retry
 end
