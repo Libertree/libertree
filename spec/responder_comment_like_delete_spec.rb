5f7a4df24f8815c31e433f6721ed38de9fd511a9
rekado <rekado@elephly.net>
2013-02-24 18:36:40 +0800

    tests: rewrite comment-like-delete specs

--- spec/responder_comment_like_delete_spec.rb
+++ spec/responder_comment_like_delete_spec.rb
@@ -1,21 +1,31 @@
 require 'spec_helper'
 
 describe Libertree::Server::Responder::CommentLike do
-  describe 'rsp_comment_like_delete' do
-    include_context 'with an INTRODUCEd and AUTHENTICATEd requester'
+  let(:subject_class) { Class.new }
+  let(:subject) { subject_class.new }
 
-    it 'with a missing id it responds with MISSING PARAMETER' do
+  before :each do
+    subject_class.class_eval {
+      include Libertree::Server::Responder::Helper
+      include Libertree::Server::Responder::CommentLike
+    }
+  end
+
+  describe 'rsp_comment_like_delete' do
+    include_context 'requester in a forest'
+
+    it 'raises MissingParameter with a missing id' do
       h = { }
-      @s.process "COMMENT-LIKE-DELETE #{h.to_json}"
-      @s.should have_responded_with_code('MISSING PARAMETER')
+      expect { subject.rsp_comment_like_delete(h) }.
+        to raise_error( Libertree::Server::MissingParameter )
     end
 
-    it 'with a blank id it responds with MISSING PARAMETER' do
+    it 'raises MissingParameter with a blank id' do
       h = {
         'id' => '',
       }
-      @s.process "COMMENT-LIKE-DELETE #{h.to_json}"
-      @s.should have_responded_with_code('MISSING PARAMETER')
+      expect { subject.rsp_comment_like_delete(h) }.
+        to raise_error( Libertree::Server::MissingParameter )
     end
 
     context 'given an existing like' do
@@ -32,15 +42,16 @@ describe Libertree::Server::Responder::CommentLike do
         @like = Libertree::Model::CommentLike.create(
           FactoryGirl.attributes_for(:comment_like, member_id: @member.id, comment_id: @comment.id)
         )
+        subject.instance_variable_set(:@server, @requester)
       end
 
-      it 'with valid data it responds with OK and deletes the local copy' do
+      it 'deletes the local copy and raises no errors with valid data' do
         like_id = @like.id
         h = {
           'id' => @like.remote_id,
         }
-        @s.process "COMMENT-LIKE-DELETE #{h.to_json}"
-        @s.should have_responded_with_code('OK')
+        expect { subject.rsp_comment_like_delete(h) }.
+          not_to raise_error
 
         Libertree::Model::CommentLike[like_id].should be_nil
       end
