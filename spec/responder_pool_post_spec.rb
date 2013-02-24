329d765a26f042851738fa29f41e7027d241806e
rekado <rekado@elephly.net>
2013-02-24 18:50:33 +0800

    tests: rewrite pool-post specs

--- spec/responder_pool_post_spec.rb
+++ spec/responder_pool_post_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
 describe Libertree::Server::Responder::PoolPost do
+  let(:subject_class) { Class.new }
+  let(:subject) { subject_class.new }
+
+  before :each do
+    subject_class.class_eval {
+      include Libertree::Server::Responder::Helper
+      include Libertree::Server::Responder::PoolPost
+    }
+  end
+
   describe 'rsp_pool_post' do
-    include_context 'with an INTRODUCEd and AUTHENTICATEd requester'
+    include_context 'requester in a forest'
 
     context 'and the responder has a record of the member, the pool and the post' do
       before :each do
@@ -15,9 +25,10 @@ describe Libertree::Server::Responder::PoolPost do
         @pool = Libertree::Model::Pool.create(
           FactoryGirl.attributes_for(:pool, member_id: @member.id, sprung: true)
         )
+        subject.instance_variable_set(:@server, @requester)
       end
 
-      it 'and a parameter is missing or blank, it responds with MISSING PARAMETER' do
+      it 'raises MissingParameter when a parameter is missing or blank' do
         h = {
           'username'   => @member.username,
           'pool_id'    => @pool.remote_id,
@@ -28,47 +39,47 @@ describe Libertree::Server::Responder::PoolPost do
         keys = h.keys
         keys.each do |key|
           h_ = h.reject { |k,v| k == key }
-          @s.process "POOL-POST #{h_.to_json}"
-          @s.should have_responded_with_code('MISSING PARAMETER')
+          expect { subject.rsp_pool_post(h_) }.
+            to raise_error( Libertree::Server::MissingParameter )
 
           h_ = h.dup
           h_[key] = ''
-          @s.process "POOL-POST #{h_.to_json}"
-          @s.should have_responded_with_code('MISSING PARAMETER')
+          expect { subject.rsp_pool_post(h_) }.
+            to raise_error( Libertree::Server::MissingParameter )
         end
       end
 
-      it "with a member username that isn't found, it responds with NOT FOUND" do
+      it "raises NotFound with a member username that isn't found" do
         h = {
           'username'   => 'nosuchusername',
           'pool_id'    => 99999999,
           'post_id'    => @post.remote_id,
           'public_key' => @requester.public_key,
         }
-        @s.process "POOL-POST #{h.to_json}"
-        @s.should have_responded_with_code('NOT FOUND')
+        expect { subject.rsp_pool_post(h) }.
+          to raise_error( Libertree::Server::NotFound )
       end
 
-      it "with a pool id that isn't found, it responds with NOT FOUND" do
+      it "raises NotFound with a pool id that isn't found" do
         h = {
           'username'   => @member.username,
           'pool_id'    => 99999999,
           'post_id'    => @post.remote_id,
           'public_key' => @requester.public_key,
         }
-        @s.process "POOL-POST #{h.to_json}"
-        @s.should have_responded_with_code('NOT FOUND')
+        expect { subject.rsp_pool_post(h) }.
+          to raise_error( Libertree::Server::NotFound )
       end
 
-      it "with a post id that isn't found, it responds with NOT FOUND" do
+      it "raises NotFound with a post id that isn't found" do
         h = {
           'username'   => @member.username,
           'pool_id'    => @pool.remote_id,
           'post_id'    => 99999999,
           'public_key' => @requester.public_key,
         }
-        @s.process "POOL-POST #{h.to_json}"
-        @s.should have_responded_with_code('NOT FOUND')
+        expect { subject.rsp_pool_post(h) }.
+          to raise_error( Libertree::Server::NotFound )
       end
 
       context 'with valid Like data, and a member that does not belong to the requester' do
@@ -79,15 +90,15 @@ describe Libertree::Server::Responder::PoolPost do
           )
         end
 
-        it 'responds with NOT FOUND' do
+        it 'raises NotFound' do
           h = {
           'username'   => @member.username,
           'pool_id'    => @pool.remote_id,
           'post_id'    => 99999999,
           'public_key' => @requester.public_key,
           }
-          @s.process "POOL-POST #{h.to_json}"
-          @s.should have_responded_with_code('NOT FOUND')
+          expect { subject.rsp_pool_post(h) }.
+            to raise_error( Libertree::Server::NotFound )
         end
       end
 
@@ -102,27 +113,27 @@ describe Libertree::Server::Responder::PoolPost do
           )
         end
 
-        it 'responds with OK' do
+        it 'raises no errors' do
           h = {
             'username'   => @member.username,
             'pool_id'    => @pool.remote_id,
             'post_id'    => @post.remote_id,
             'public_key' => @requester.public_key,
           }
-          @s.process "POOL-POST #{h.to_json}"
-          @s.should have_responded_with_code('OK')
+          expect { subject.rsp_pool_post(h) }.
+            not_to raise_error
         end
       end
 
-      it 'with valid data it responds with OK' do
+      it 'raises no errors with valid data' do
         h = {
           'username'   => @member.username,
           'pool_id'    => @pool.remote_id,
           'post_id'    => @post.remote_id,
           'public_key' => @requester.public_key,
         }
-        @s.process "POOL-POST #{h.to_json}"
-        @s.should have_responded_with_code('OK')
+        expect { subject.rsp_pool_post(h) }.
+          not_to raise_error
       end
     end
   end
