bc9f22eaee4eb38e33bfeb2b2388481028b260d1
rekado <rekado@elephly.net>
2013-02-24 18:55:15 +0800

    tests: rewrite pool-post-delete specs

--- spec/responder_pool_post_delete_spec.rb
+++ spec/responder_pool_post_delete_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
 describe Libertree::Server::Responder::PoolPost do
+  let(:subject_class) { Class.new }
+  let(:subject) { subject_class.new }
+
+  before :each do
+    subject_class.class_eval {
+      include Libertree::Server::Responder::Helper
+      include Libertree::Server::Responder::PoolPost
+    }
+  end
+
   describe 'rsp_pool_post_delete' do
-    include_context 'with an INTRODUCEd and AUTHENTICATEd requester'
+    include_context 'requester in a forest'
 
     context 'and the responder has a record of the member, the pool and the post' do
       before :each do
@@ -15,9 +25,10 @@ describe Libertree::Server::Responder::PoolPost do
         @pool = Libertree::Model::Pool.create(
           FactoryGirl.attributes_for(:pool, member_id: @member.id, sprung: true)
         )
+        subject.instance_variable_set(:@server, @requester)
       end
 
-      it 'and a parameter is missing or blank, it responds with MISSING PARAMETER' do
+      it 'raises MissingParameter when a parameter is missing or blank' do
         h = {
           'username'   => @member.username,
           'pool_id'    => @pool.remote_id,
@@ -28,60 +39,60 @@ describe Libertree::Server::Responder::PoolPost do
         keys = h.keys
         keys.each do |key|
           h_ = h.reject { |k,v| k == key }
-          @s.process "POOL-POST-DELETE #{h_.to_json}"
-          @s.should have_responded_with_code('MISSING PARAMETER')
+          expect { subject.rsp_pool_post_delete(h_) }.
+            to raise_error( Libertree::Server::MissingParameter )
 
           h_ = h.dup
           h_[key] = ''
-          @s.process "POOL-POST-DELETE #{h_.to_json}"
-          @s.should have_responded_with_code('MISSING PARAMETER')
+          expect { subject.rsp_pool_post_delete(h_) }.
+            to raise_error( Libertree::Server::MissingParameter )
         end
       end
 
-      it "with a member username that isn't found, it responds with NOT FOUND" do
+      it "raises NotFound with a member username that isn't found" do
         h = {
           'username'   => 'nosuchusername',
           'pool_id'    => 99999999,
           'post_id'    => @post.remote_id,
           'public_key' => @requester.public_key,
         }
-        @s.process "POOL-POST-DELETE #{h.to_json}"
-        @s.should have_responded_with_code('NOT FOUND')
+        expect { subject.rsp_pool_post_delete(h) }.
+          to raise_error( Libertree::Server::NotFound )
       end
 
-      it "with a pool id that isn't found, it responds with NOT FOUND" do
+      it "raises NotFound with a pool id that isn't found" do
         h = {
           'username'   => @member.username,
           'pool_id'    => 99999999,
           'post_id'    => @post.remote_id,
           'public_key' => @requester.public_key,
         }
-        @s.process "POOL-POST-DELETE #{h.to_json}"
-        @s.should have_responded_with_code('NOT FOUND')
+        expect { subject.rsp_pool_post_delete(h) }.
+          to raise_error( Libertree::Server::NotFound )
       end
 
-      it "with a post id that isn't found, it responds with NOT FOUND" do
+      it "raises NotFound with a post id that isn't found" do
         h = {
           'username'   => @member.username,
           'pool_id'    => @pool.remote_id,
           'post_id'    => 99999999,
           'public_key' => @requester.public_key,
         }
-        @s.process "POOL-POST-DELETE #{h.to_json}"
-        @s.should have_responded_with_code('NOT FOUND')
+        expect { subject.rsp_pool_post_delete(h) }.
+          to raise_error( Libertree::Server::NotFound )
       end
 
       # For now, deletion silently fails in this case.
 
-      # it 'with valid and recognized IDs, but no pool association, it responds with NOT FOUND' do
+      # it 'raises NotFound with valid and recognized IDs, but no pool association' do
         # h = {
           # 'username'   => @member.username,
           # 'pool_id'    => @pool.remote_id,
           # 'post_id'    => @post.remote_id,
           # 'public_key' => @requester.public_key,
         # }
-        # @s.process "POOL-POST-DELETE #{h.to_json}"
-        # @s.should have_responded_with_code('NOT FOUND')
+        # expect { subject.rsp_pool_post_delete(h) }.
+        #   to raise_error( Libertree::Server::NotFound )
       # end
 
       context 'with valid data and a known association' do
@@ -95,10 +106,10 @@ describe Libertree::Server::Responder::PoolPost do
           }
         end
 
-        it 'responds with OK and dissociates the post from the pool' do
+        it 'raises no error and dissociates the post from the pool' do
           @pool.includes?(@post).should be_true
-          @s.process "POOL-POST-DELETE #{@h.to_json}"
-          @s.should have_responded_with_code('OK')
+          expect { subject.rsp_pool_post_delete(@h) }.
+            not_to raise_error
           @pool.dirty.includes?(@post).should be_false
         end
       end
