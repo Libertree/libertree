2d412650e32830201ffc7f78c58d3361b2c8b133
rekado <rekado@elephly.net>
2013-02-25 17:53:14 +0800

    move XML helpers to separate file

--- lib/libertree/client.rb
+++ lib/libertree/client.rb
@@ -2,9 +2,12 @@ require 'base64'
 require 'openssl'
 require 'socket'
 require 'blather/client/client'
+require_relative 'xml/helper'
 
 module Libertree
   class Client
+    include Libertree::XML::Helper
+
     # @param [Hash] params Paramaters Hash
     # @option params [String] :public_key A public RSA key, the partner of the private key
     # @option params [String] :private_key A private RSA key, the partner of the public key
@@ -50,41 +53,6 @@ module Libertree
 
     def log_error(s)
       log s, 'ERROR'
-    end
-
-    def build_stanza( target, params )
-      stanza = Blather::Stanza::Iq.new(:set, target)
-      content = "<libertree xmlns=\"libertree\">#{params_to_xml(params)}</libertree>"
-      stanza.add_child content
-      stanza
-    end
-
-    def params_to_xml(elem)
-      case elem
-      when Array
-        # TODO: this squashes arrays
-        # before:
-        #   :abc => [1,2,3,4]
-        # after:
-        #   <abc>1234</abc>
-        #
-        # what this be better?
-        #   <abc>
-        #     <item>1</item>
-        #     <item>2</item>
-        #     <item>3</item>
-        #     <item>4</item>
-        #   </abc>
-        elem.flat_map {|i| params_to_xml(i) }.join('')
-      when Hash
-        elem.reduce("") do |acc,i|
-          acc << ("<#{i[0]}>"+ params_to_xml(i[1]) +"</#{i[0]}>")
-          acc
-        end
-      else
-        # TODO: encode entities
-        elem.to_s
-      end
     end
 
     public
---
+++ lib/libertree/xml/helper.rb
@@ -0,0 +1,42 @@
+require 'blather/stanza'
+
+module Libertree
+  module XML
+    module Helper
+      def build_stanza( target, params )
+        stanza = Blather::Stanza::Iq.new(:set, target)
+        content = "<libertree xmlns=\"libertree\">#{params_to_xml(params)}</libertree>"
+        stanza.add_child content
+        stanza
+      end
+
+      def params_to_xml(elem)
+        case elem
+        when Array
+          # TODO: this squashes arrays
+          # before:
+          #   :abc => [1,2,3,4]
+          # after:
+          #   <abc>1234</abc>
+          #
+          # would this be better?
+          #   <abc>
+          #     <item>1</item>
+          #     <item>2</item>
+          #     <item>3</item>
+          #     <item>4</item>
+          #   </abc>
+          elem.flat_map {|i| params_to_xml(i) }.join('')
+        when Hash
+          elem.reduce("") do |acc,i|
+            acc << ("<#{i[0]}>"+ params_to_xml(i[1]) +"</#{i[0]}>")
+            acc
+          end
+        else
+          # TODO: encode entities
+          elem.to_s
+        end
+      end
+    end
+  end
+end
