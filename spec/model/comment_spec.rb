5a7813364f1f08572ced4fe65846d5e3910fc4ed
Pistos <github.pistos@purepistos.net>
2013-03-17 12:49:11 -0400

    Failing specs for new way that search will work on comments.

--- spec/model/comment_spec.rb
+++ spec/model/comment_spec.rb
@@ -43,4 +43,43 @@ describe Libertree::Model::Comment do
       end
     end
   end
+
+  describe '.search' do
+    before :each do
+      Libertree::DB.dbh.execute 'TRUNCATE comments CASCADE'
+      @p1 = new_comment 'This is a test comment.'
+      @p2 = new_comment 'This is also a test comment.'
+      @p3 = new_comment 'This is a comment.'
+      @p4 = new_comment 'That is a comment.'
+      @p5 = new_comment 'me and my hat'
+      @p6 = new_comment "'Twas brillig, and the slithy toves Did gyre and gimble in the wabe"
+      @p7 = new_comment 'This was not good.  However, that is.'
+    end
+
+    # Expect the given query to return the given Array of Comments (in any order).
+    def expect_search_match(query, comments)
+      results = Libertree::Model::Comment.search(query)
+      expect(comments - results).to be_empty, "Expected #{query.inspect} to also match #{(comments-results).map(&:text)}"
+      expect(results - comments).to be_empty, "Expected #{query.inspect} not to match #{(results-comments).map(&:text)}"
+    end
+
+    it 'matches comments to single word queries' do
+      expect_search_match 'This', [ @p1, @p2, @p3, @p7 ]
+      expect_search_match 'is', [ @p1, @p2, @p3, @p4, @p7 ]
+      expect_search_match 'test', [ @p1, @p2 ]
+      expect_search_match 'test', [ @p1, @p2 ]
+      expect_search_match 'That', [ @p4, @p7 ]
+      expect_search_match 'me', [ @p5 ]
+      expect_search_match 'Me', [ @p5 ]
+      expect_search_match 'ME', [ @p5 ]
+      expect_search_match 'Twas', [ @p6 ]
+    end
+
+    it 'matches comments to multi-word queries' do
+      expect_search_match 'This is', [ @p1, @p2, @p3, @p7 ]
+      expect_search_match 'is this', [ @p1, @p2, @p3, @p7 ]
+      expect_search_match 'that is', [ @p4, @p7 ]
+      expect_search_match 'comment this', [ @p1, @p2, @p3 ]
+    end
+  end
 end
