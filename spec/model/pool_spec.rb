ca25d284c13e3a22ce712ba7ed7f218161ad7f1e
Pistos <github.pistos@purepistos.net>
2013-03-19 23:11:05 -0400

    Added spec: "Adding a post to a pool does not notify the author"

--- spec/model/pool_spec.rb
+++ spec/model/pool_spec.rb
@@ -28,4 +28,27 @@ describe Libertree::Model::Pool do
     expect(subject.pool).to eq spring
     expect(subject.post).to eq post
   end
+
+  it 'Adding a post to a pool does not notify the author' do
+    account_poster = Libertree::Model::Account.create( FactoryGirl.attributes_for(:account) )
+    member_poster = account_poster.member
+
+    account_pooler = Libertree::Model::Account.create( FactoryGirl.attributes_for(:account) )
+    member_pooler = account_pooler.member
+
+    post = Libertree::Model::Post.create(
+      FactoryGirl.attributes_for( :post, member_id: member_poster.id, text: 'post to be sprung' )
+    )
+
+    pool = Libertree::Model::Pool.create(
+      FactoryGirl.attributes_for( :pool, member_id: member_pooler.id, name: 'Post Feed', sprung: false )
+    )
+
+    expect(account_poster.notifications.count).to eq 0
+
+    pool << post
+
+    account_poster = Libertree::Model::Account[ account_poster.id ]
+    expect(account_poster.notifications.count).to eq 0
+  end
 end
