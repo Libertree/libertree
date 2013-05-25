8db3c31a19f5920dd1f5ea038ec1104e32251540
rekado <rekado@elephly.net>
2013-05-25 08:04:52 +0800

    parse XML as it comes through the socket
    We cannot just use a simple line-based protocol to receive stanzas through
    the socket as it cannot deal with newline characters embedded in messages.
    As we have to parse the incoming data anyway, we can do so incrementally
    using a SAX parser as we receive the stream through the socket.

--- lib/libertree/server/relay.rb
+++ lib/libertree/server/relay.rb
@@ -5,20 +5,96 @@ module Libertree
     # from the XMPP server is passed back through the socket to the
     # relay client (e.g. the job processor) to consume.
     module Relay
-      include EM::P::LineText2
 
-      def receive_line(line)
-        return  if line.empty?
-
-        # Blather::Stanza.parse does not throw a
-        # clearly recognisable exception on parse failure,
-        # so it has its own rescue block.
-        begin
-          stanza = Blather::Stanza.parse(line)
-        rescue
-          Libertree::Server.log_error "XMPP relay: not a valid stanza: #{line}"
-          return
+      # The following class implementation has been adapted
+      # from Blather::Stream::Parser
+      class Parser < Nokogiri::XML::SAX::Document
+        def initialize(caller)
+          @caller = caller
+          @parser = Nokogiri::XML::SAX::PushParser.new self
+          @parser.options =
+            Nokogiri::XML::ParseOptions::DEFAULT_XML |
+            Nokogiri::XML::ParseOptions::NOENT
+          @current = nil
+          @namespaces = {}
+          @namespace_definitions = []
         end
+
+        def receive_data(string)
+          @parser << string
+        end
+
+        def start_element_namespace(elem, attrs, prefix, uri, namespaces)
+          args = [elem]
+          args << @current.document  if @current
+          node = Blather::XMPPNode.new *args
+          node.document.root = node  unless @current
+
+          attrs.each do |attr|
+            node[attr.localname] = attr.value
+          end
+
+          ns_keys = namespaces.map { |pre, href| pre }
+          @namespace_definitions.push []
+          namespaces.each do |pre, href|
+            next  if @namespace_definitions.flatten.include?(@namespaces[[pre, href]])
+            ns = node.add_namespace(pre, href)
+            @namespaces[[pre, href]] ||= ns
+          end
+
+          if prefix && ! ns_keys.include?(prefix)
+            @namespaces[[prefix, uri]] ||= node.add_namespace(prefix, uri)
+          end
+          node.namespace = @namespaces[[prefix, uri]]
+
+          @current << node  if @current
+          @current = node
+        end
+
+        def end_element_namespace(elem, prefix, uri)
+          if @current.parent != @current.document
+            # travel up the stack
+            @namespace_definitions.pop
+            @current = @current.parent
+          else
+            # handle stanza and implode
+            @caller.handle_stanza @current.to_stanza
+          end
+        end
+
+        def characters(chars='')
+          if @current
+            @current << Nokogiri::XML::Text.new(chars, @current.document)
+          end
+        end
+
+        def warning(msg)
+          Libertree::Server.log "XMPP relay: #{msg}", "WARN"
+        end
+
+        def error(msg)
+          raise ParseError.new(msg)
+        end
+      end
+
+      def receive_data(string)
+        @parser.receive_data string
+      rescue ParseError => e
+        Libertree::Server.log_error "XMPP relay parse: #{e}"
+      end
+
+      def post_init
+        Libertree::Server.log "XMPP relay: process connected", "INFO"
+        @parser = Parser.new self
+      end
+
+      def unbind
+        Libertree::Server.log "XMPP relay: process disconnected", "INFO"
+      end
+
+      def handle_stanza(stanza)
+        # throw away the old parser
+        @parser = Parser.new self
 
         begin
           # TODO: expose client through proper interface
@@ -29,16 +105,8 @@ module Libertree
             send_data response
           end
         rescue => e
-          Libertree::Server.log_error "XMPP relay: #{e}"
+          Libertree::Server.log_error "XMPP relay: #{e}; stanza: #{stanza.inspect}"
         end
-      end
-
-      def post_init
-        Libertree::Server.log "XMPP relay: process connected", "INFO"
-      end
-
-      def unbind
-        Libertree::Server.log "XMPP relay: process disconnected", "INFO"
       end
     end
   end
