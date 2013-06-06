8d65595108ddc38146c16b34ccde0a735a4e200e
rekado <rekado@elephly.net>
2013-06-06 09:55:08 +0800

    server display name: prefer domain over ip

--- lib/libertree/model/server.rb
+++ lib/libertree/model/server.rb
@@ -2,7 +2,7 @@ module Libertree
   module Model
     class Server < M4DBI::Model(:servers)
       def name_display
-        self.name_given || self.ip
+        self.name_given || self.domain || self.ip || "(unknown)"
       end
 
       def forests
