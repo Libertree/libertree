beea16a2a93df95bf69baedd59e08cce9a9337cf
Pistos <github.pistos@purepistos.net>
2013-06-21 11:09:51 -0400

    Revert "new table: remote storage connection for accounts"
    This reverts commit f86e940901df9d442855cf82c95ff13c2a4ff969.

--- migrations/2013-06-07-2205-remote-storage-connections.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE remote_storage_connections(
-      id SERIAL
-    , time_created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
-    , account_id INTEGER NOT NULL REFERENCES accounts(id)
-    , access_token VARCHAR(256)
-    , handle VARCHAR(128)
-    , PRIMARY KEY(id)
-    , CONSTRAINT remote_storage_connections_account_id_key UNIQUE (account_id)
-);
