675e95caf508a8fdcd9c3bd602a10cbe9c5fff6c
Pistos <github.pistos@purepistos.net>
2013-06-21 11:09:45 -0400

    Revert "save storage URL for remote storage"
    This reverts commit d014f61a04fa08cf1b64c31a93549ddf9713f11b.

--- migrations/2013-06-07-2205-remote-storage-connections.sql
+++ migrations/2013-06-07-2205-remote-storage-connections.sql
@@ -4,7 +4,6 @@ CREATE TABLE remote_storage_connections(
     , account_id INTEGER NOT NULL REFERENCES accounts(id)
     , access_token VARCHAR(256)
     , handle VARCHAR(128)
-    , storage_url VARCHAR(512)
     , PRIMARY KEY(id)
     , CONSTRAINT remote_storage_connections_account_id_key UNIQUE (account_id)
 );
