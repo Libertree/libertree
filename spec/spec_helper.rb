94a067c4eaeae3d33fddf6dd8defdb9539f8cec4
rekado <rekado@elephly.net>
2013-07-15 21:41:20 +0800

    remove unused MockServer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,41 +14,7 @@ Libertree::DB.dbh
 require 'libertree/server'
 require_relative 'factories'
 
-class MockServer
-  include Libertree::Server
-
-  attr_reader :responses
-
-  def initialize
-    @responses = []
-    @public_key = 'public-key-of-mock-server'
-    Libertree::Server.log_handle = File.open( 'test-server.log', 'a+' )
-  end
-
-  def respond(data)
-    @responses << data.to_json
-  end
-
-  def close_connection_after_writing
-    # noop in testing
-  end
-
-  def response
-    JSON.parse @responses[-1]
-  end
-
-  def has_responded_with?(hash)
-    response.should == hash
-  end
-
-  def has_responded_with_code?(code)
-    has = ( response['code'] == code )
-    if ! has
-      $stderr.puts "Expected #{code.inspect}, got #{response['code'].inspect}.  Error message: #{response['message'].inspect}"
-    end
-    has
-  end
-end
+Libertree::Server.log_handle = File.open( 'test-server.log', 'a+' )
 
 if ENV['LIBERTREE_ENV'] != 'test'
   $stderr.puts "Refusing to run specs in a non-test environment.  Comment out the exit line if you know what you're doing."
@@ -57,15 +23,10 @@ end
 
 RSpec.configure do |config|
   config.before(:each) do
-    @s = MockServer.new
     Libertree::Server.conf = {}
     Libertree::DB.dbh.execute "SET client_min_messages TO 'warning';"
     Libertree::DB.dbh.execute 'TRUNCATE posts CASCADE'
     Libertree::DB.dbh.execute 'TRUNCATE servers CASCADE'
-  end
-
-  config.after(:each) do
-    @s.unbind
   end
 end
 
