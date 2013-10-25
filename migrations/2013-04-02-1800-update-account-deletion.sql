9a9285e36c154c496c9f9ff0736805b1b7b01170
rekado <rekado@elephly.net>
2013-10-25 10:40:35 +0800

    remove function definitions from migrations

--- migrations/2013-04-02-1800-update-account-deletion.sql
+++ migrations/2013-04-02-1800-update-account-deletion.sql
@@ -9,35 +9,3 @@ CREATE INDEX river_posts_by_river_id       ON river_posts(river_id);
 CREATE INDEX river_posts_by_post_id        ON river_posts(post_id);
 CREATE INDEX comments_by_member_id         ON comments(member_id);
 CREATE INDEX comment_likes_by_comment_id   ON comment_likes(comment_id);
-
-CREATE OR REPLACE FUNCTION delete_cascade_member(member_id INTEGER) RETURNS void AS $$
-    SELECT delete_cascade_pool(p.id)
-           FROM pools p WHERE p.member_id = $1;
-    SELECT delete_cascade_post(p.id)
-           FROM posts p WHERE p.member_id = $1;
-    SELECT delete_cascade_post_like(l.id)
-           FROM post_likes l WHERE l.member_id = $1;
-    SELECT delete_cascade_comment(c.id)
-           FROM comments c WHERE c.member_id = $1;
-    SELECT delete_cascade_comment_like(l.id)
-           FROM comment_likes l WHERE l.member_id = $1;
-
-    DELETE FROM chat_messages WHERE to_member_id = $1 OR from_member_id = $1;
-    DELETE FROM contact_lists_members WHERE member_id = $1;
-    DELETE FROM message_recipients
-           WHERE member_id = $1
-           OR message_id IN (SELECT id FROM messages WHERE sender_member_id = $1);
-    DELETE FROM messages WHERE sender_member_id = $1;
-    DELETE FROM
-        messages m
-    WHERE
-        NOT EXISTS (
-            SELECT 1
-            FROM message_recipients mr
-            WHERE mr.message_id = m.id
-        )
-    ;
-    DELETE FROM profiles WHERE member_id = $1;
-
-    DELETE FROM members WHERE id = $1;
-$$ LANGUAGE SQL;
--- migrations/2013-06-07-2205-remote-storage-connections.sql
+++ migrations/2013-06-07-2205-remote-storage-connections.sql
@@ -8,30 +8,3 @@ CREATE TABLE remote_storage_connections(
     , PRIMARY KEY(id)
     , CONSTRAINT remote_storage_connections_account_id_key UNIQUE (account_id)
 );
-
-CREATE OR REPLACE FUNCTION delete_cascade_account(account_id INTEGER) RETURNS void AS $$
-    UPDATE invitations SET inviter_account_id = NULL WHERE inviter_account_id = $1;
-    DELETE FROM invitations                WHERE account_id = $1;
-    DELETE FROM sessions_accounts          WHERE account_id = $1;
-    DELETE FROM notifications              WHERE account_id = $1;
-    DELETE FROM post_subscriptions         WHERE account_id = $1;
-    DELETE FROM posts_read                 WHERE account_id = $1;
-    DELETE FROM posts_hidden               WHERE account_id = $1;
-    DELETE FROM remote_storage_connections WHERE account_id = $1;
-    DELETE FROM
-        contact_lists_members clm
-    USING
-        contact_lists cl
-    WHERE
-        cl.account_id = $1
-        AND clm.contact_list_id = cl.id
-    ;
-    DELETE FROM contact_lists WHERE account_id = $1;
-
-    SELECT delete_cascade_river(r.id)
-           FROM rivers r WHERE r.account_id = $1;
-    SELECT delete_cascade_member(m.id)
-           FROM members m WHERE m.account_id = $1;
-
-    DELETE FROM accounts WHERE id = $1;
-$$ LANGUAGE SQL;
