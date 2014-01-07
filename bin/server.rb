3cba8cc66144d4099a25e09388ef7f925ea66ac9
rekado <rekado@elephly.net>
2014-01-07 22:11:21 +0800

    Expect database config to be passed
    This change makes it easier to package the backend as it can
    be configured to use an existing database configuration,
    possibly installed by the model library or some other package.

--- bin/server.rb
+++ bin/server.rb
@@ -1,15 +1,22 @@
 require 'libertree/db'
 
 if ARGV[0].nil?
-  $stderr.puts "#{$0} <config.yaml>"
+  $stderr.puts "#{$0} <config.yaml> <database.yaml>"
   exit 1
+end
+
+if ARGV[1].nil?
+  $stderr.puts "no database configuration file given; assuming #{File.dirname( __FILE__ ) }/../database.yaml"
+  db_config = "#{File.dirname( __FILE__ ) }/../database.yaml"
+else
+  db_config = ARGV[1]
 end
 
 ########################
 # FIXME: M4DBI wants us to connect to the db before defining models.  As model
 # definitions are loaded when 'libertree/server' is required, we have to do
 # this first.
-Libertree::DB.load_config "#{File.dirname( __FILE__ ) }/../database.yaml"
+Libertree::DB.load_config db_config
 Libertree::DB.dbh
 ########################
 
