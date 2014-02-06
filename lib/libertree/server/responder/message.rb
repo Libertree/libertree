71a23791c6ccf72b59e16376a43c229e5195e154
rekado <rekado@elephly.net>
2014-02-06 22:02:05 +0800

    move DM forwarding via email to libertree-model

--- lib/libertree/server/responder/message.rb
+++ lib/libertree/server/responder/message.rb
@@ -12,43 +12,28 @@ module Libertree
             ]
             fail_if_nil sender_member, "Unrecognized member username: #{params['username'].inspect}"
 
-            members = params['recipients'].reduce({:local => [], :remote => []}) { |ms, recipient|
+            members = params['recipients'].reduce({}) { |ms, recipient|
               origin = Model::Server[ domain: recipient['origin'] ]
               if origin
                 member = Model::Member['username' => recipient['username'], 'server_id' => origin.id]
-                ms[:remote] << member  if member
+                ms << member  if member
               elsif origin.nil? && recipient['origin'] == Server.conf['domain']
                 # origin is this local server
                 account = Model::Account['username' => recipient['username']]
                 if account
-                  ms[:local] << account.member
+                  ms << account.member
                 end
               end
 
               ms
             }
 
-            member_ids = members[:remote].map(&:id) + members[:local].map(&:id)
+            member_ids = members.map(&:id)
             message = Libertree::Model::Message.create_with_recipients(
               sender_member_id: sender_member.id,
               text: params['text'],
               recipient_member_ids: member_ids
             )
-
-            # forward via email for those local recipients who requested it
-            members[:local].map(&:account).select {|a| a.email && a.forward_dms_via_email }.
-              each do |account|
-                Libertree::Model::Job.create(
-                  task: 'email',
-                  params: {
-                    'to'      => account.email,
-                    'pubkey'  => account.pubkey,
-                    'subject' => '[Libertree] Direct message', # TODO: translate
-                    'body'    => "#{sender_member.handle} wrote:\n\n#{params['text']}"
-                  }.to_json
-                )
-            end
-
           rescue PGError => e
             fail InternalError, "Error in #{__method__}: #{e.message}", nil
           end
