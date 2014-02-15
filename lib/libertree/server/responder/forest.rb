278e55d1c3334084defecb1d793480c9809ac0b7
rekado <rekado@elephly.net>
2014-02-15 20:33:50 +0800

    adapt forest responder to new client XML format

--- lib/libertree/server/responder/forest.rb
+++ lib/libertree/server/responder/forest.rb
@@ -20,10 +20,10 @@ module Libertree
               )
             end
 
-            trees = params['trees']['domain'].reject { |tree_name|
-              tree_name == Server.conf['domain']
+            trees = params['trees'].reject { |tree|
+              tree['domain'] == Server.conf['domain']
             }
-            forest.set_trees_by_domain trees
+            forest.set_trees_by_domain trees.map {|t| t['domain']}
           rescue PGError => e
             fail InternalError, "Error on FOREST request: #{e.message}", nil
           end
