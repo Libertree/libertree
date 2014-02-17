dce12e2d5064609c1fe63aa8d20c46ffb5033981
rekado <rekado@elephly.net>
2014-02-17 17:45:24 +0800

    remove unused migration methods

--- lib/libertree/db.rb
+++ lib/libertree/db.rb
@@ -28,33 +28,5 @@ module Libertree
         password: config['password']
       )
     end
-
-    def self.ensure_migration_table_exists
-      result = dbh.execute( "SELECT 1 FROM pg_tables WHERE schemaname='public' AND tablename = 'schema_migrations'" )
-      return  if result.has_data?
-
-      dbh.execute( %{
-        CREATE TABLE schema_migrations (
-          filename VARCHAR(1024) NOT NULL UNIQUE
-        )
-      } )
-    end
-
-    def self.migrate
-      ensure_migration_table_exists
-      migrations_run = dbh.execute( "SELECT filename FROM schema_migrations" ).as(:Struct).fetch(:all).map(&:filename)
-
-      __DIR__ = File.dirname( __FILE__ )
-      Dir["#{__DIR__}/../../../migrations/*.sql"].sort.each do |migration|
-        filename = File.basename( migration )
-        if ! migrations_run.include?( filename )
-          dbh.transaction do
-            dbh.execute "INSERT INTO schema_migrations ( filename ) VALUES ( ? )", filename
-            system "psql -v ON_ERROR_STOP=1 -U #{config['username']} -f '#{migration}' -1 #{config['database']}" or
-              dbh.rollback
-          end
-        end
-      end
-    end
   end
 end
