95d6e06b2df580a16a7d886a3285705b6c8061ac
rekado <rekado@elephly.net>
2014-08-03 21:55:11 +0200

    build project Gemfile from sub-projects

---
+++ packaging/build_gemfile.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+class GemfileMerger
+  def initialize(dirs)
+    @hash = dirs.reduce({}) do |gemfile, dir|
+      contents = self.parse("../#{dir}/Gemfile")
+      gemfile.merge!(contents) do |key,oldval,newval|
+        (oldval + newval).uniq
+      end
+      gemfile
+    end
+  end
+
+  def parse(filename)
+    group = nil
+    lines = IO.readlines(filename)
+    lines.reduce({}) do |res, item|
+      if m = item.match(/^group '(?<name>.*)' do/)
+        group = m['name']
+        res[group] = []
+      elsif  m = item.match(/^end/)
+        group = nil
+      else
+        if group
+          res[group] << item
+        else
+          res[:root] = []   if res[:root].nil?
+          res[:root] << item
+        end
+      end
+
+      res
+    end
+  end
+
+  def print(except=["development"])
+    @hash.each_pair do |key, stuff|
+      if except.include? key
+        next
+      else
+        if key == :root
+          ignore = true
+        else
+          puts "group '#{key}' do"
+        end
+        puts stuff.join
+        puts "end"  unless ignore
+      end
+    end
+  end
+end
+
+# merge the Gemfiles of all sub-projects
+gemfile = GemfileMerger.new ['libertree-model-rb', 'libertree-backend-rb', 'libertree-client-rb']
+gemfile.print
