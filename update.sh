ee7cb43a2716bd0fce2881f7e93c1dfd3735cf28
rekado <rekado@elephly.net>
2014-09-07 08:34:00 +0200

    update included libertree-db subtree

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git stash
-for dir in libertree-client-rb libertree-frontend-ramaze libertree-backend-rb libertree-model-rb; do
+for dir in libertree-db libertree-client-rb libertree-frontend-ramaze libertree-backend-rb libertree-model-rb; do
     echo "updating subtree: $dir"
     git subtree pull --squash --prefix=$dir https://github.com/Libertree/$dir master
 done
