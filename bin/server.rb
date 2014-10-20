2bdd217fa44c7389e2a7a1c4960790707d674a53
rekado <rekado@elephly.net>
2014-10-20 21:19:38 +0200

    Squashed 'libertree-backend-rb/' changes from 5934209..7d92847
    7d92847 add service management script
    c87824e load defaults also in job-processor lib
    f37de9e rename config.yaml.example to defaults.yaml and load automatically
    ecc4e95 ahem, we are no longer using M4DBI
    f8e6f3d default: path to private.key
    3777649 raise error if private.key file doesn't exist
    600065e default to tmp directory relative to backend directory
    218af80 create directory holding socket if it doesn't exist
    18bce01 default: libertree.localhost for local deployments
    3be3028 drop quotes from string settings
    af19fa2 define pids directory and log path per default
    91b789e create log and pid directories recursively if needed
    
    git-subtree-dir: libertree-backend-rb
    git-subtree-split: 7d92847da43e9d3b01ccced86a8d6eed8161ff85

--- bin/server.rb
+++ bin/server.rb
@@ -13,7 +13,7 @@ else
 end
 
 ########################
-# FIXME: M4DBI wants us to connect to the db before defining models.  As model
+# Sequel wants us to connect to the db before defining models.  As model
 # definitions are loaded when 'libertree/server' is required, we have to do
 # this first.
 Libertree::DB.load_config db_config
